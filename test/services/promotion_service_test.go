6874a9a986f369d3efebf05c2f7aacd537e0864e
chethan.a.aux <chethan.a.aux@gmail.com>
2020-11-30 23:18:34 +0530

    Added ApplyPromos tests

--- test/services/promotion_service_test.go
+++ test/services/promotion_service_test.go
@@ -1,7 +1,36 @@
 package services
 
-import "testing"
+import (
+	"checkoutService/models/data"
+	"checkoutService/models/dto"
+	"checkoutService/services"
+	"testing"
+)
 
-func TestShouldApply(t *testing.T) {
-	
+func TestShouldApplyApplicablePromotions(t *testing.T) {
+	products := []dto.Product{{
+		Name:  "Macbook Pro",
+		Price: 1,
+	},{
+		Name:  "Macbook Pro",
+		Price: 1,
+	}}
+	cart := dto.ShoppingCart{
+		Products:   products,
+		TotalPrice: 1,
+	}
+	promo := data.PromotionDetails{
+		PromotionName:      "RaspberryPi",
+		ProductName:        "Macbook Pro",
+		MinimumQuantity:    1,
+		DiscountPercentage: 0,
+		AdditionalDetails: map[string]string{
+			"FreeProduct": "Raspberry Pi",
+		},
+	}
+	cartAfterPromo := services.ApplyPromos(cart, []data.PromotionDetails{promo})
+
+	if len(cartAfterPromo.Products) != 3 || cartAfterPromo.TotalPrice != cart.TotalPrice {
+		t.Errorf("Applicable promotion not applied!")
+	}
 }
